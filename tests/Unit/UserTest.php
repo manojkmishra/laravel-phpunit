67f3cdb66ede6d39a178dcd9bc1ba806b57ea79b
manojkmishra <manojmishra571980@gmail.com>
2018-09-07 08:45:38 +1000

    user creation unit test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Tests\Unit;
-
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -16,9 +16,14 @@ class UserTest extends TestCase
     public function testExample()
     {
         //$this->assertTrue(true);
-        $response = $this->get('/'); //can be any api or url
+      //  $response = $this->get('/'); //can be any api or url
 
-        $response->assertStatus(404); //this test fails
+       // $response->assertStatus(404); //this test fails
        // $response->assertStatus(200); //---this test pass
+
+       $user=factory(User::class)->create();
+       $response=$this->actingAs($user)->get('/');
+       $response->assertSuccessful();
+       $user->forceDelete();
     }
 }
