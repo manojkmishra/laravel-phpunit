106575fd4adcf512a51688b9b364447e5a7a73c5
manojkmishra <manojmishra571980@gmail.com>
2018-09-07 08:02:06 +1000

    new test(UserTest) created in unit and feature

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,18 +3,18 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class UserTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testExample()
     {
         $this->assertTrue(true);
-       // $this->assertTrue(flase);
     }
 }
